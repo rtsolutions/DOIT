cc65ad98f1992be17eb0b04a73d3c00ea7bca742
Corey <corey@realtimesites.com>
2014-08-11 14:19:44 -0500

    commenting like a madman

--- git/DOIT/EmergencyPreparedness/EmergencyPreparedness/QuizQuestionViewController.m
+++ git/DOIT/EmergencyPreparedness/EmergencyPreparedness/QuizQuestionViewController.m
@@ -34,6 +34,7 @@
 
 - (void)setupQuestions
 {
+    // Each question has as many as 8 answers.
     NSString *answer1 = self.currentQuestion.answer1;
     NSString *answer2 = self.currentQuestion.answer2;
     NSString *answer3 = self.currentQuestion.answer3;
@@ -43,6 +44,7 @@
     NSString *answer7 = self.currentQuestion.answer7;
     NSString *answer8 = self.currentQuestion.answer8;
     
+    // Check to make sure each answer exists before adding it to self.answers
     [self.answers addObject:answer1];
     if (answer2)
     {
@@ -73,16 +75,20 @@
         [self.answers addObject:answer8];
     }
     
-    
+    // correctanswer is a string. That way multiple correct answers can be stored as strings separated by spaces,
+    // and then they can be parsed out.
     NSArray *correctAnswersTemp = [self.currentQuestion.correctanswer componentsSeparatedByString:@" "];
     for (NSString *numberString in correctAnswersTemp)
     {
         NSInteger value = [numberString integerValue];
-        value--;
+        
+        value--;    // Decrement to get the index rather than the answer number
+        
         NSNumber *number = [NSNumber numberWithInt:value];
         [self.correctAnswerIndexes addObject:number];
     }
     
+    // Add the correct answers to an array self.correctAnswers
     for (NSNumber *index in self.correctAnswerIndexes)
     {
         [self.correctAnswers addObject:self.answers[[index integerValue]]];
@@ -94,6 +100,7 @@
     return [self.answers count];
 }
 
+/// Calcuate how much space each row is going to need, then add 25 points of padding
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:17.0]};
@@ -106,17 +113,24 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    // For some reason the app couldn't find "Cell" so I used "Cell2" instead
     static NSString *CellIdentifier = @"Cell2";
     UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     NSString *answer = self.answers[indexPath.row];
     
     cell.textLabel.text = answer;
+    
+    // Word wrap the answers, and let them use as many lines as they need
     [cell.textLabel setLineBreakMode:NSLineBreakByWordWrapping];
     cell.textLabel.numberOfLines = 0;
+    
+    // Move the text 70 points to the right (10 points x 7 indents = 70 points)
     cell.indentationLevel = 7;
     cell.indentationWidth = 10;
     
+    // If we've already clicked on "ANSWER QUESTION", only the correct answers are in the table.
+    // Add a green checkmark next to them.
     if (self.showingAnswers == YES)
     {
         UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 5, 30, 30)];
@@ -131,9 +145,13 @@
     return cell;
 }
 
+/// Switch between adding a green checkmark and adding the answer to chosen answers, and removing
+/// the checkmark and removing the answer from chosen answers
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    // Remove
     if (self.showingAnswers == NO)
     {
         BOOL removedImage = NO;
@@ -155,6 +173,7 @@
             }
         }
         
+        // Add
         if (removedImage == NO)
         {
             UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 5, 30, 30)];
@@ -168,28 +187,27 @@
             [self.chosenAnswerIndexes addObject:chosenAnswer];
         }
     }
-
-    
-    
 }
 
-
+/// Action performed on button press
 - (IBAction)answerQuestion:(id)sender
 {
+    
     if ([self.answerButton.titleLabel.text isEqual:@"FINISH QUIZ"])
     {
+        // Pop back to the home view controller, then navigate back to the quiz menu
         [[NSNotificationCenter defaultCenter] postNotificationName:@"popAndPushToQuiz" object:nil];
-        
     }
+    
     else if ([self.answerButton.titleLabel.text isEqual:@"NEXT QUESTION"])
     {
-        
+        // Navigate to self, and display the data for the next question
         [self performSegueWithIdentifier:@"navigateToSelf" sender:self];
-        
     }
     
     else if ([self.answerButton.titleLabel.text isEqual:@"ANSWER QUESTION"])
     {
+        // Only show the correct answers
         self.showingAnswers = YES;
         [self.answers removeAllObjects];
         for (NSString *answer in self.correctAnswers)
@@ -198,9 +216,11 @@
         }
         [self.tableView reloadData];
         
+        // Make sets with the correct answers and the user's chosen answers
         NSSet *set1 = [NSSet setWithArray:self.correctAnswerIndexes];
         NSSet *set2 = [NSSet setWithArray:self.chosenAnswerIndexes];
         
+        // Check if they're equal. If they are, display the green checkmark. If not, keep the gray checkmark.
         if ([set1 isEqualToSet:set2])
         {
             self.checkmarkImageView.image = [UIImage imageNamed:@"greenCheck.png"];
@@ -210,6 +230,7 @@
             self.checkmarkImageView.image = [UIImage imageNamed:@"grayCheck.png"];
         }
         
+        // If we're at the end of the array, prepare to finish the quiz. Otherwise, set up for another question
         if ((self.currentQuestionIndex + 1) == [self.questionsArray count])
         {
             [self.answerButton setTitle:@"FINISH QUIZ" forState:UIControlStateNormal];
@@ -261,24 +282,41 @@
     }
 }
 
-- (void)viewDidLoad
+
+- (void) setupView
 {
-    [super viewDidLoad];
+    // Initialize arrays to hold the data
     self.answers = [NSMutableArray new];
     self.correctAnswers = [NSMutableArray new];
     self.correctAnswerIndexes = [NSMutableArray new];
     self.chosenAnswerIndexes = [NSMutableArray new];
+    
+    // Get the question at the current index
     [self getCurrentQuestion];
+    
+    // Sort the question into answers and correct answers, and keep track of indexes
     [self setupQuestions];
+    
+    
+    // Add data to all of the views in the viewcontroller
     self.titleLabel.text = [@"Quiz: " stringByAppendingString:self.titleString];
     [self.answerButton setTitle:@"ANSWER QUESTION" forState:UIControlStateNormal];
     [self.answerButton.titleLabel setHidden:NO];
     self.checkmarkImageView.image = [UIImage imageNamed:@"grayCheck.png"];
     self.questionTextView.text = self.currentQuestion.title;
+    [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+    
+    // Show the question number, eg "5 of 11"
     NSString *questionNumberLabelString = [NSString stringWithFormat:@"%d", self.currentQuestionIndex + 1];
     questionNumberLabelString = [questionNumberLabelString stringByAppendingString:@" of "];
     questionNumberLabelString = [questionNumberLabelString stringByAppendingString:[NSString stringWithFormat:@"%d",[self.questionsArray count]]];
     self.questionNumberLabel.text = questionNumberLabelString;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setupView];
     // Do any additional setup after loading the view.
 }
 
