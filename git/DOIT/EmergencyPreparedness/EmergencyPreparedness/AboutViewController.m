6e840c065936f852efecee9838b253c798b77f8e
Corey <corey@realtimesites.com>
2014-08-18 12:21:27 -0500

    Added “About This App”

---
+++ git/DOIT/EmergencyPreparedness/EmergencyPreparedness/AboutViewController.m
@@ -0,0 +1,57 @@
+//
+//  AboutViewController.m
+//  EmergencyPreparedness
+//
+//  Created by rts on 8/18/14.
+//  Copyright (c) 2014 RTS. All rights reserved.
+//
+
+#import "AboutViewController.h"
+#import "Constants.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+
+    
+    self.versionLabel.text = [NSString stringWithFormat:@"Version: %@", [[[NSBundle mainBundle] infoDictionary]objectForKey:@"CFBundleShortVersionString"]];
+    
+    self.databaseLabel.text = [@"Database: " stringByAppendingString: AWSSampleDynamoDBTableName];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
